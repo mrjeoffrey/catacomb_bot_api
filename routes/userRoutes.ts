330775463d970f07fab3919600d0197466f04476
Moana <jeoffreyb88@gmail.com>
2024-11-23 00:47:37 +0900

    Cors & Auth

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,11 +1,18 @@
-import express from 'express';
-import { getUserInfo, completeTask, openChest, refer } from '../controllers/userController';
+import express from "express";
+import {
+  getUserInfo,
+  completeTask,
+  openChest,
+  refer,
+  getUsers,
+} from "../controllers/userController";
 
 const router = express.Router();
 
-router.get('/info', getUserInfo);
-router.post('/complete-task', completeTask);
-router.post('/open-chest', openChest);
-router.post('/refer', refer);
+router.get("/", getUsers);
+router.get("/info", getUserInfo);
+router.post("/complete-task", completeTask);
+router.post("/open-chest", openChest);
+router.post("/refer", refer);
 
 export default router;
