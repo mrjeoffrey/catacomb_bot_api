3e12648ede5064509785b0a904d30bb0c1c0c52e
Moana <jeoffreyb88@gmail.com>
2024-12-04 00:27:32 +0900

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 80;
+export const PORT = process.env.PORT || 5000;
 export const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/telegram_game";
 export const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
