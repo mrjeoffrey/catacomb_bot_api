ad1dde5847047f68dee4794761688ee58b404ec5
Moana <jeoffreyb88@gmail.com>
2025-01-14 18:16:38 -0800

    Update userController.ts

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -531,7 +531,6 @@ export const getUserInfo = async (req: Request, res: Response) => {
     const { rankings, currentUserRank } = await getRankings(user);
     // Convert the Mongoose document to a plain JavaScript object
     const userPlainObject = user.toObject();
-    console.log(new Date(), "Current Time")
     // Add the season_xp, season_gold, totalSeasonXP, rank, and valid referrals
     const userInfo = {
       ...userPlainObject,
