b85ded3951baf436c2df9d0f5b99b7769b7ec588
Moana <jeoffreyb88@gmail.com>
2025-01-22 16:52:24 -0800

    Update taskController.ts

--- controllers/taskController.ts
+++ controllers/taskController.ts
@@ -302,6 +302,9 @@ export const taskProofingOrder = async (req: Request, res: Response) => {
           existingTask.validation_status = "unchecked";
         }
       } else {
+        if(imageUrl === "" || imageUrl === undefined || imageUrl === null) {
+          res.status(400).json({ message: "Please add a screenshot" })
+        }
         // Add new task if it doesn't exist
         user.task_done.push({
           task_id,
