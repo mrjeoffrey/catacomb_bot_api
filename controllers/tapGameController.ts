f97173bdfde24435f81a71d47e29b5289af86afd
Moana <jeoffreyb88@gmail.com>
2025-01-29 19:56:41 -0800

    Update tapGameController.ts

--- controllers/tapGameController.ts
+++ controllers/tapGameController.ts
@@ -365,13 +365,17 @@ export const ticketToTaps = async (req: Request, res: Response) => {
   if (user.tickets_remaining < 1) {
     return res.status(400).json({ message: "Not enough tickets" });
   }
-  // if(user.current_available_taps > 0) {
-  //   return res.status(200).json({ message: "Taps are remaining", current_available_taps: user.current_available_taps });
-  // }
+  if(user.current_available_taps > 0) {
+    return res.status(200).json({ message: "Taps are remaining", current_available_taps: user.current_available_taps });
+  }
   user.tickets_remaining--;
   const tapLevel = await getUserTapLevelByUserXp(user.xp);
   user.current_available_taps = tapLevel.tap_limit_per_ticket;
   await user.save();
+  return res.status(200).json({
+    message: "Converted a ticket to available Taps",
+    current_available_taps: user.current_available_taps,
+  });
 };
 
 export const getAllTapGameLevels = async (req: Request, res: Response) => {
