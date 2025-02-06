946cf2c3a03e561db4b323847b21b440adad26cf
Moana <jeoffreyb88@gmail.com>
2025-02-06 14:02:54 -0800

    Removing unecessary chest history for season 2

--- app.ts
+++ app.ts
@@ -197,7 +197,7 @@ seedInitialData();
 
 loadLevelsInMemory();
 
-// removeUnnecessaryChestListInSpecificPeriod(1)
+removeUnnecessaryChestListInSpecificPeriod(2)
 // recalcAllUserInfo();
 
 // Schedule the checkUserActivityAndSendMessages function to run every hour
